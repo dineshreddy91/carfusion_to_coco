34023cdc44ccbc39d60e32cac77e2a9757da8829
Your Name <you@example.com>
2019-05-22 15:01:38 -0400

    update

--- carfusion_coco_setup.sh
+++ carfusion_coco_setup.sh
@@ -1,3 +1,7 @@
-#python download_carfusion.py
-python carfusion2coco.py --path_dir ./datasets/carfusion/train/ --label_dir gt --image_dir image_jpg --output_dir ./datasets/carfusion/annotations/ --output_filename car_keypoints_train.json
-python carfusion2coco.py --path_dir ./datasets/carfusion/test/ --label_dir gt --image_dir image_jpg --output_dir  ./datasets/carfusion/annotations/ --output_filename car_keypoints_test.json
+## download the data from the carfusion website
+
+python download_carfusion.py
+
+## scripts to convert hte data from carfusion to coco format
+python carfusion2coco.py --path_dir ./datasets/carfusion/train/ --label_dir gt --image_dir images_jpg --output_dir ./datasets/carfusion/annotations/ --output_filename car_keypoints_train.json
+python carfusion2coco.py --path_dir ./datasets/carfusion/test/ --label_dir gt --image_dir images_jpg --output_dir  ./datasets/carfusion/annotations/ --output_filename car_keypoints_test.json
